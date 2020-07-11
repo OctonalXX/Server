9bee401b793074e117c6471881bfa30a0d827b0f
OctonalXX <54666576+OctonalXX@users.noreply.github.com>
2020-07-10 20:17:08 -0700

    Update Hyper-V-Enabler.bat

--- Hyper-V-Enabler.bat
+++ Hyper-V-Enabler.bat
@@ -2,8 +2,8 @@
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 title Hyper-V Enabler
 pushd "%~dp0"
-dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum >hyper-v.txt
-for /f %%i in ('findstr /i . hyper-v.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
-del hyper-v.txt
+dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum >hyper-v.config
+for /f %%i in ('findstr /i . hyper-v.config 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
+del hyper-v.config
 Dism /online /enable-feature /featurename:Microsoft-Hyper-V -All /LimitAccess /ALL
 exit
